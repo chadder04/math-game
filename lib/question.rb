fc8d184703d37c71c2e2813b66ae275e86f6d920
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-10 13:29:22 -0700

    Added module MathGame

--- lib/question.rb
+++ lib/question.rb
@@ -1,21 +1,23 @@
-class Question
-  attr_accessor :num1, :num2, :operation
+module MathGame
+  class Question
+    attr_accessor :num1, :num2, :operation
 
-  def initialize
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
-    @operation = [:+, :-, :/, :*].sample
-  end
+    def initialize
+      @num1 = rand(1..20)
+      @num2 = rand(1..20)
+      @operation = [:+, :-, :/, :*].sample
+    end
 
-  def get_question
-    "What is the solution to the following problem : #{num1} #{operation} #{num2} = ? "
-  end
+    def get_question
+      "What is the solution to the following problem : #{num1} #{operation} #{num2} = ? "
+    end
 
-  def check_answer(answer)
-    if answer.to_i == @num1.send(@operation, @num2).to_i
-      return true
-    else 
-      return false
+    def check_answer(answer)
+      if answer.to_i == @num1.send(@operation, @num2).to_i
+        return true
+      else 
+        return false
+      end
     end
   end
 end

7aaf5b6d434af31db42393d4a579955cc724a2f7
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-10 13:13:17 -0700

    Setup rotate_players, and get_current_player_index methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,25 @@
 module MathGame
   class Turn
 
-    def initialize(player, question)
-      @player = player
-      @question = question
+    def initialize(players)
+      @players = players
+      @current_player_index = 0
+    end
+
+    def rotate_players
+      if (@current_player_index >= (@players.size-1))
+        @current_player_index = 0
+      else
+        @current_player_index += 1
+      end
+    end
+
+    def get_current_player
+      @players[@current_player_index]
+    end
+
+    def get_current_player_index
+      @current_player_index
     end
 
   end
